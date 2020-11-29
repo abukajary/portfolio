177203c9336282470314f42b969435ef325d0b3c
abukajary <70243848+abukajary@users.noreply.github.com>
2020-11-29 22:01:30 +0600

    add main files
    added main files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  let path = window.location.pathname;
+  let page = path.split("/").pop();
+
+  switch (page) {
+    case "index.html":
+      let el = document.getElementById('a_home');
+      el.style.borderBottom = "1px solid black";
+      break;
+    case "about.html":
+      let el1 = document.getElementById('a_about');
+      el1.style.borderBottom = "1px solid black";
+      break;
+    default:
+      let el2 = document.getElementById('a_contact');
+      el2.style.borderBottom = "1px solid black";
+      break;
+  }
+
+});
