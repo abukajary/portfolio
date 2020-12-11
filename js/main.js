ce6fe22b3b970a2fb3d8c194cd40d133d90d502e
abukajary <70243848+abukajary@users.noreply.github.com>
2020-12-12 01:44:45 +0600

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  let path = window.location.pathname;
-  let page = path.split("/").pop();
-
-  switch (page) {
-    case "https:abukajary.github.ioportfolio":
-      let el = document.getElementById('a_home');
-      el.style.borderBottom = "1px solid black";
-      break;
-    case "about.html":
-      let el1 = document.getElementById('a_about');
-      el1.style.borderBottom = "1px solid black";
-      break;
-    default:
-      let el2 = document.getElementById('a_contact');
-      el2.style.borderBottom = "1px solid black";
-      break;
-  }
-
-});
+const EMAIL = "abubakir02061995@gmail.com"
+let subject = "TITLE";
+let body = "hellow"
+function sendMail() {
+    window.open('mailto:' + EMAIL + '?subject=' + subject + '&body=' + body);
+}
